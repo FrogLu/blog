d2eb9e0775ca5bc019eb70c3281aeec959f9ac52
周潇韵 <zgqyzxy@163.com>
2018-12-02 23:36:29 +0800

    enable serviceWorker

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,14 @@
 module.exports = {
     title: 'FrogLu',
     description: 'Cockroachs apprentice',
+    serviceWorker: true,
     ga: 'UA-129512845-1',
     themeConfig: {
         repo: 'FrogLu/FrogLu.github.io',
         lastUpdated: 'Last Updated',
+        serviceWorker: {
+            updatePopup: {message: 'New content is available.', buttonText: 'Refresh'}
+        },
         docsDir: 'docs',
         algolia: { apiKey: '744bca5e7ed206ef244e700fc4014300', indexName: 'FrogLu'},
         tags: true,
